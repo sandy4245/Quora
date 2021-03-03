1ed7fbc917fbb7d938a2545a1e15401fb2f6b42b
root <nikhilbisht837@gmail.com>
2021-03-03 22:34:53 +0530

    Create QuestionEntity dao class

---
+++ Trello_quora/quora-service/src/main/java/com/upgrad/quora/service/entity/QuestionEntity.java
@@ -0,0 +1,109 @@
+package com.upgrad.quora.service.entity;
+
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.awt.*;
+import java.io.Serializable;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "QUESTION", schema = "quora")
+public class QuestionEntity implements Serializable {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "UUID")
+    @Size(max = 200)
+    private String uuid;
+
+    @Column(name = "CONTENT")
+    @Size(max = 500)
+    private String content;
+
+    @Column(name = "DATE")
+    private ZonedDateTime date;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "USER_ID")
+    private UserEntity user;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "question")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private List<AnswerEntity> answerEntities = new ArrayList<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public ZonedDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(ZonedDateTime date) {
+        this.date = date;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    public List<AnswerEntity> getAnswerEntities() {
+        return answerEntities;
+    }
+
+    public void setAnswerEntities(List<AnswerEntity> answerEntities) {
+        this.answerEntities = answerEntities;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return new EqualsBuilder().append(this, obj).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(this).hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+}
