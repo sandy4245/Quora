f11b530ba744f27c042df1e7e4108203e13af01f
root <nikhilbisht837@gmail.com>
2021-03-03 19:00:04 +0530

    Create UserEntity dao class and add Getter and Setters

---
+++ Trello_quora/quora-service/src/main/java/com/upgrad/quora/service/entity/UserEntity.java
@@ -0,0 +1,189 @@
+package com.upgrad.quora.service.entity;
+
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Entity
+@Table(name = "USERS", schema = "quora")
+public class UserEntity {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "UUID")
+    @Size(max = 64)
+    private String uuid;
+
+    @Column(name = "FIRST_NAME")
+    @NotNull
+    @Size(max = 200)
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    @NotNull
+    @Size(max = 200)
+    private String lastName;
+
+
+    @Column(name = "USERNAME")
+    @NotNull
+    @Size(max = 200)
+    private String userName;
+
+    @Column(name = "EMAIL")
+    @NotNull
+    @Size(max = 200)
+    private String email;
+
+    @Column(name = "PASSWORD")
+    @NotNull
+    private String password;
+
+    @Column(name = "SALT")
+    @NotNull
+    @Size(max = 200)
+    private String salt;
+
+    @Column(name = "COUNTRY")
+    private String country;
+
+    @Column(name = "ABOUT_ME")
+    @Size(max = 500)
+    private String aboutMe;
+
+    @Column(name = "DATE OF BIRTH")
+    @NotNull
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private Date dateOfBirth;
+
+    @Column(name = "ROLE")
+    private String role;
+
+    @Column(name = "MOBILE NUMBER")
+    @NotNull
+    @Size(max = 20)
+    private String contactNumber;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAboutMe() {
+        return aboutMe;
+    }
+
+    public void setAboutMe(String aboutMe) {
+        this.aboutMe = aboutMe;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return new EqualsBuilder().append(this, obj).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(this).hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+
+}
